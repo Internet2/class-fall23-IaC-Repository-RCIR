4a9d775307fb133e7a86fe006b095b09abba61ce
kluuking <71826000+kluuking@users.noreply.github.com>
2023-10-24 18:11:33 -0500

    Update s3.tf

--- Usecases/Intelligent Storage Solution/s3.tf
+++ Usecases/Intelligent Storage Solution/s3.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 # =================================
 variable "bucket-name" {
-  default = "<Bucket with Intelligent tiering>" 
+  default = "<Bucket with Intelligent tiering>"  #name of the bucket generated 
 }
 #================================
 
@@ -15,19 +15,24 @@ resource "aws_s3_bucket" "create-s3-bucket" {
 
   bucket = "${var.bucket-name}"
 
-  acl = "private"
+  acl = "private" ##Standard to private ACL 
 
   lifecycle_rule {
-    id = "<LifeCycle Name>"  
+    id = "<LifeCycle Name>"   ## set name for your rule
     enabled = true
     transition {
       days = 30      
-      storage_class = "STANDARD_IA"
+      storage_class = "STANDARD_IA" ## move your bucket to a standard_IA after 30 days of inactivity
     }
 
     transition {
       days          = 60
-      storage_class = "GLACIER"
+      storage_class = "GLACIER_IR"  ## Move your bucket to a Glacier after 30 days of inactivity
+    }
+    transition {
+      days          = 180
+      storage_class = "DEEP_ARCHIVE" ## Move your bucket to a Deep Glacier after 180 days of inactivity
+    }
     }
 
   }
@@ -37,7 +42,7 @@ resource "aws_s3_bucket" "create-s3-bucket" {
   }
 
   tags = {
-    Enviroment: "LifeCycle Controlled" 
+    Environment: "LifeCycle Controlled" ## Tagging convention to track what this belongs to
   }
 
   server_side_encryption_configuration {
