38edbeb2314161f7aa5957bac2e98ef6e951436e
kluuking <71826000+kluuking@users.noreply.github.com>
2023-10-24 20:29:52 -0500

    Update s3.tf

--- Usecases/Intelligent Storage Solution/s3.tf
+++ Usecases/Intelligent Storage Solution/s3.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "XXX"
-  secret_key = "XXX"
+  access_key = "XXX"   ## Input your own Access Key
+  secret_key = "XXX"   ## Input your own Secret Key
 
 }
 
 # =================================
 variable "bucket-name" {
-  default = "<Bucket with Intelligent tiering>"  #name of the bucket generated 
+  default = "<Bucket with Intelligent tiering>"  ## name of the bucket generated 
 }
 #================================
 
@@ -15,13 +15,13 @@ resource "aws_s3_bucket" "create-s3-bucket" {
 
   bucket = "${var.bucket-name}"
 
-  acl = "private" ##Standard to private ACL 
+  acl = "private" ## Standard to private ACL 
 
   lifecycle_rule {
     id = "<LifeCycle Name>"   ## set name for your rule
     enabled = true
     transition {
-      days = 30      
+      days          = 30      
       storage_class = "STANDARD_IA" ## move your bucket to a standard_IA after 30 days of inactivity
     }
 
@@ -33,12 +33,16 @@ resource "aws_s3_bucket" "create-s3-bucket" {
       days          = 180
       storage_class = "DEEP_ARCHIVE" ## Move your bucket to a Deep Glacier after 180 days of inactivity
     }
+
+    expiration {
+      days = 1825  ## delete bucket after 5 years of inactive 
+    }
     }
 
   }
 
   versioning {
-    enabled = true
+    enabled = true ## enabling version for preserve, retrieve, and restore version. 
   }
 
   tags = {
@@ -48,7 +52,7 @@ resource "aws_s3_bucket" "create-s3-bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
+        sse_algorithm = "aws:kms"  ## Apply KMS encryption
       }
     }
   }
