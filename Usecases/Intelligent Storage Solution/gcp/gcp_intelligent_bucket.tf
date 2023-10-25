4cc76d97187fd34ae3e1ce721b0c592111559817
Ian Kaufman <iankgt40>
2023-10-25 13:50:15 -0700

    More syntax and logic clean up

--- Usecases/Intelligent Storage Solution/gcp/gcp_intelligent_bucket.tf
+++ Usecases/Intelligent Storage Solution/gcp/gcp_intelligent_bucket.tf
@@ -2,22 +2,22 @@
 # with standard storage
 
 resource "google_storage_bucket" "auto-expire" {
-  name          = "intelligent_tiering_bucket2"
-#  project_id    = "my-gcp-project" /* apparently not needed
+  name          = "intelligent_tiering_bucket"
+  project_id    = "my-gcp-project"
   location      = "US"
 
   public_access_prevention = "inherited"  /* inherits rules from organization's policy constraint if it exists , option is "enforced"
 
   force_destroy = true  /* removes all objects in bucket when delete
 
-  iam_members 
+  iam_members (
     {
-       role = "roles/storage.admin" /* full read/write to all tiers for PI -- maybe the service_account?
+       role = "roles/storage.admin /* full read/write to all tiers for PI -- maybe the service_account?
        member = "user:spam@example.com"  /* Can be comma delimited list, or by group -> ["group:foo-admins@example.com"]
     }
 
     {
-       role = "roles/storage.admin" /* read/write to all but ARCHIVE for lab manager
+       role = "roles/storage. /* read/write to all but ARCHIVE for lab manager
        member = "user:spam@example.com"
     }
 
@@ -25,8 +25,10 @@ resource "google_storage_bucket" "auto-expire" {
        role = "roles/storage.objectViewer"  /* read only general lab member
        member = "user:spam@example.com"
     }
+  }
 
-  lifecycle_rule = { 
+  lifecycle_rule = {
+    {
       action = {
         type          = "SetStorageClass"
         storage_class = "STANDARD"
@@ -35,9 +37,10 @@ resource "google_storage_bucket" "auto-expire" {
         age                   = "0"
       }
     }
- 
+ }
 
-  lifecycle_rule = { 
+  lifecycle_rule = {
+    {
      condition = {
         age                   = "30"
       }
@@ -46,9 +49,10 @@ resource "google_storage_bucket" "auto-expire" {
         storage_class = "NEARLINE"
       }
     }
- 
+ }
 
   lifecycle_rule = {
+    {
      condition = {
         age                   = "90"
       }
@@ -57,9 +61,10 @@ resource "google_storage_bucket" "auto-expire" {
         storage_class = "COLDLINE"
       }
     }
- 
+ }
 
   lifecycle_rule = {
+    {
      condition = {
         age                   = "365"
       }
@@ -68,9 +73,10 @@ resource "google_storage_bucket" "auto-expire" {
         storage_class = "ARCHIVE"
       }
     }
-  
+  }
 
   lifecycle_rule = {
+    {
       action = {
         type          = "Delete"
       }
@@ -78,7 +84,7 @@ resource "google_storage_bucket" "auto-expire" {
         age                   = "1826" /* Delete afer 5 years
       }
     }
-    
+  }  
 
   versioning {
     enabled        = true
@@ -91,7 +97,7 @@ resource "google_storage_bucket" "auto-expire" {
 /*
  * Optionally we could use autoclass
  *
- * autoclass = {
+ * autoclass = }
  *    enabled = true
  *    terminal_storage_class = "ARCHIVE"
  * }
