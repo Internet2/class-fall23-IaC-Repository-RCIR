65ca3817bf7f5b359d7be35f09a6c605882fb8a0
Riley Conroy <rpconroy@ucar.edu>
2023-10-25 11:58:32 -0600

    Updated paths

--- Usecases/NCAR Serverless/process.bash
+++ Usecases/NCAR Serverless/process.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
-for file in `cat ERA5_TMP_2m.txt`; do 
+for file in `cat ERA5_TMP_2m_disk.txt`; do 
+    echo $file
+
     wget $file
+    file=`basename $file`
     ./netcdf_subset.py $file subset_$file elon=85 wlon=75 nlat=70 slat=40
+    break
 done
