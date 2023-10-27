f718ee857498babef78933c87a6f93b25a72b324
btassone <brandontassone@gmail.com>
2023-10-27 12:25:37 -0400

    Added gke

---
+++ cicd/github/gcp/terraform/gke.tf
@@ -0,0 +1,73 @@
+module "gke_auth" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version = "24.1.0"
+  depends_on   = [module.gke]
+  project_id   = var.project_id
+  location     = module.gke.location
+  cluster_name = module.gke.name
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-${var.env_name}"
+}
+
+module "gcp-network" {
+  source       = "terraform-google-modules/network/google"
+  version      = "6.0.0"
+  project_id   = var.project_id
+  network_name = "${var.network}-${var.env_name}"
+
+  subnets = [
+    {
+      subnet_name   = "${var.subnetwork}-${var.env_name}"
+      subnet_ip     = var.main_subnet
+      subnet_region = var.region
+    },
+  ]
+
+  secondary_ranges = {
+    "${var.subnetwork}-${var.env_name}" = [
+      {
+        range_name    = var.ip_range_pods_name
+        ip_cidr_range = var.ip_range_pods
+      },
+      {
+        range_name    = var.ip_range_services_name
+        ip_cidr_range = var.ip_range_services
+      },
+    ]
+  }
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  version                = "24.1.0"
+  project_id             = var.project_id
+  name                   = "${var.cluster_name}-${var.env_name}"
+  regional               = true
+  region                 = var.region
+  network                = module.gcp-network.network_name
+  subnetwork             = module.gcp-network.subnets_names[0]
+  ip_range_pods          = var.ip_range_pods_name
+  ip_range_services      = var.ip_range_services_name
+
+  node_pools = [
+    {
+      name                      = var.node_pool_name
+      machine_type              = var.node_machine_type
+      node_locations            = var.node_locations
+      min_count                 = var.min_nodes
+      max_count                 = var.max_nodes
+      disk_size_gb              = var.node_disk_size
+    },
+  ]
+}
